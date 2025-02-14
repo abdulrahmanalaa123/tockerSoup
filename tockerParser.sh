6dcc130c46b866b7e4cd5c261f82007cb32a4e38
abdelrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2025-02-14 22:20:18 +0200

    fixing parser and ensuring functions run properly

--- tockerParser.sh
+++ tockerParser.sh
@@ -1,36 +1,56 @@
 # man 5 systemd.resource-control
 # default CPUQuota=5% IOreadbandwith=20M/s by default B,K,M,G,T IOwritebandwith=  MemoryMin=1G(requested)B,K,M,G,T MemoryMax=2G(requested)B,K,M,G,T(absolute limit)  MemoryHigh2G(softcap)=B,K,M,G,T AllowedCPUs=1,2 (specify which cpus are specified)
 # either enable accounting on the userslice to enable quoting the services i presume 
-# tocker container run  --cpuquota [0-100] --iowrite [num(B|K|M|G|T)] --ioread [num(B|K|M|G|T)] --memmax [num(B|K|M|G|T)] --memmin [num(B|K|M|G|T)] --memhigh [num(B|K|M|G|T)] <image_name> <command> # images are associate with an entryPoint can be done when pulling
-# tocker container exec <container_id> <command>
-# tocker container start <container_id> # keep running the image on the latest entrypoint
-# tocker container rm -rf <container_id> # -r remove the ocntainer alongside its respective image -f removes the conatainer and stops it abruptly
-# tocker image rm -f <image_name> #remove 
-# tocker container stop <container_id>
-# tocker image get <image_name>
-# tocker container la ls l
-# tocker image la ls l 
 
-if [[ $# -gt 0 ]]; 
-then
-       	while [ "${1:0:2}" == '--' ]; 
-	do 
-		OPTION=${1:2}; [[ $OPTION =~ = ]] 
-		declare "BOCKER_${OPTION/=*/}=${OPTION/*=/}" 
-		declare "BOCKER_${OPTION}=x"
-	       	shift; 
-	done
-fi
 
-container_run () {}
-container_exec () {}
-container_start () {}
-container_rm () {}
-container_stop () {}
-container_ls () {}
-image_get () {}
-image_rm () {}
-image_ls () {}
+container_run () {
+	echo running
+
+	if [[ $# -gt 0 ]]; 
+	then
+		while [ "${1:0:2}" == '--' ]; 
+		do 
+			OPTION=${1:2}; 
+			if [[ $OPTION =~ = ]] 
+			then
+				declare "TOCKER_${OPTION/=*/}=${OPTION/*=/}" | declare "TOCKER_${OPTION}=x"
+				shift; 
+			fi
+		done
+	fi
+}
+
+container_exec () {
+	echo execing
+}
+
+container_start () {
+	echo starting
+}
+
+container_rm () {
+	echo removing_cont
+}
+
+container_stop () {
+	echo stopping
+}
+
+container_ls () {
+	echo lsing_cont
+}
+
+image_get () {
+	echo getting
+}
+
+image_rm () {
+	echo removing_image
+}
+
+image_ls () {
+	echo lsing_image
+}
 
 parser () {
 	if [[ $1 =~ (image) ]];
@@ -60,7 +80,7 @@ tocker_help () {
 }
 
 container_parser () {
-	case $1) in
+	case $1 in
 		run|"exec"|"stop"|start|"rm"|"ls")
 			container_"$1" "${@:2}"
 			;;
@@ -71,7 +91,7 @@ container_parser () {
 }
 
 image_parser () {
-	case $1) in
+	case $1 in
 		get|"rm"|"ls")
 			image_"$1" "${@:2}"
 			;;
@@ -80,4 +100,14 @@ image_parser () {
 			;;
 	esac	
 }
+
+parser container run
+parser image get
+parser container rm
+parser container exec
+parser container stop
+parser container start
+parser image rm
+parser image ls
+parser container ls
 
